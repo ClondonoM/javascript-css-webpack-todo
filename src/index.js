3709a09219f4ab0dc3b9c85cbfeddfd2bc72ec89
Carlos Londono <c.londono.m@gmail.com>
2022-04-22 18:53:11 -0500

    build code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import "./styles.css";
 import { Todo, TodoList } from "./classes";
+import { createTodoHtml } from "./js/components";
 
 const todoList = new TodoList();
 
-const task = new Todo("Learn JS");
+const task = new Todo("Learn JS ");
 
 todoList.newTodo(task);
 
 console.log(todoList);
+
+createTodoHtml(task);
