f7ea94e8052cdc831daa13778dc108615a7e859b
Carlos Londono <c.londono.m@gmail.com>
2022-04-21 19:54:58 -0500

    modifying components

---
+++ src/js/components.js
@@ -0,0 +1,19 @@
+//html references
+const divTodoList = document.querySelector(".todo-list");
+
+export const createTodoHtml = (todo) => {
+  const htmlTodo = `
+          <li class="completed" data-id="abc">
+						<div class="view">
+							<input class="toggle" type="checkbox" checked>
+							<label>${todo.task}</label>
+							<button class="destroy"></button>
+						</div>
+						<input class="edit" value="Create a TodoMVC template">
+					</li>`;
+
+  const div = document.createElement("div");
+  div.innerHTML = htmlTodo;
+  divTodoList.append(div);
+  return div;
+};
